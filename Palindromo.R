c418809b7b14c53ede301a718d2575d4ee824252
Iván Paredes Reséndiz <ivanresendiz25@gmail.com>
2020-04-06 15:41:24 -0500

    Add files via upload

---
+++ Palindromo.R
@@ -0,0 +1,11 @@
+#https://www.cofece.mx/wp-content/uploads/2018/10/Libro-CPC-PoderyBienestar-ver4.pdf
+
+#/*Esta version del programa tiene la version final de las regiones geograficas (clave numerica en parentesis):
+#1.- NOROESTE (seis): Baja California(2), Baja California Sur(3), Chihuahua(8), Durango(10), Sinaloa(25), Sonora(26)
+#2.- NORESTE (tres): Coahuila(5), Nuevo Leon(19), Tamaulipas(28)
+#3.- CENTRO_NORTE (cinco): Aguascalientes(1), Guanajuato(11), Queretaro(22), San Luis Potosi(24), Zacatecas(32)
+#4.- CENTRO_SUR (tres): Ciudad de Mexico(9), Estado de Mexico(15), Morelos(17)
+#5.- SUROESTE (tres): Chiapas(7), Guerrero(12), Oaxaca(20)
+#6.- SURESTE (cuatro): Campeche(4), Quintana Roo(23), Tabasco(27), Yucatan(31)
+#7.- OESTE (cuatro): Colima(6), Jalisco(14), Michoacan(16), Nayarit(18)
+#8.- ESTE (cuatro): Hidalgo(13), Puebla(21), Tlaxcala(29), Veracruz(30)*/
